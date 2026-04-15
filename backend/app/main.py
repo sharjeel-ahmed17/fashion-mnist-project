dd813901bb82c4f8663dba68087d49cc44b8c57e
sharjeel-ahmed17 <sharjeelahmedofficial2@gmail.com>
2026-04-15 23:15:31 +0500

    .......

--- backend/app/main.py
+++ backend/app/main.py
@@ -35,7 +35,7 @@ model = load_model(MODEL_PATH)
 def root():
     return {"message": "Fashion CNN API is running 🚀"}
 @app.get("/health")
-def health_care123uuuu():
+def health_care123uuuu123():
     return {"message": "health is good 🚀"}
 
 
